3cc0dfa5395e1176fe1478d4ea9ceeba6c5488ce
GolamMorshed <gmriyal@gmail.com>
2021-12-10 23:58:59 +0600

    login-registration
    function done
    1. login-done
    2. registration- have some bug
    3. logout-done
    4. get login user information-done

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +17,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+    Route::get('/home', 'HomeController@index')->name('home');
 });
