46a931f75ed7a1a618d68168e63a35808286aa20
GolamMorshed <gmriyal@gmail.com>
2021-12-11 00:07:52 +0600

    rearrange the code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-// use Illuminate\Support\Facades\Validator;
 use Validator;
 use Auth;
 
@@ -35,22 +34,29 @@ class AuthController extends Controller
 
     //SIGN IN
     public function login(Request $request){
+
         $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string',
             'remember_me' => 'boolean'
         ]);
+
         $credentials = request(['email', 'password']);
+
         if(!Auth::attempt($credentials))
             return response()->json([
                 'message' => 'Unauthorized'
             ], 401);
+
         $user = $request->user();
         $tokenResult = $user->createToken('Personal Access Token');
         $token = $tokenResult->token;
+
         if ($request->remember_me)
             $token->expires_at = Carbon::now()->addWeeks(1);
+
         $token->save();
+
         return response()->json([
             'access_token' => $tokenResult->accessToken,
             'token_type' => 'Bearer',
@@ -62,7 +68,9 @@ class AuthController extends Controller
 
     //LOGOUT
     public function logout(Request $request){
+
         $request->user()->token()->revoke();
+
         return response()->json([
             'message' => 'Successfully logged out'
         ]);
@@ -70,6 +78,7 @@ class AuthController extends Controller
 
     //SHOW USER INFORMATION
     public function user(Request $request){
+
         return response()->json($request->user());
     }
 
