cd3fcd110fafd8b835680c494577b220aed7f686
GolamMorshed <gmriyal@gmail.com>
2021-12-25 22:10:57 +0600

    Create files

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\WireTransferController;
 use App\Http\Controllers\PaymentRequestController;
 use App\Http\Controllers\LoanController;
 use App\Http\Controllers\FixedDepoController;
+use App\Http\Controllers\SupportTicketController;
 use App\Http\Controllers\CurrencyController;
 use App\Http\Controllers\FDRPlanController;
 use App\Http\Controllers\UserListController;
@@ -77,9 +78,15 @@ Route::get('/list_of_loan_request',[LoanController::class,'index']);
 Route::post('/loan_request',[LoanController::class,'store']);
 Route::get('/user_loan_request_list/{id}',[LoanController::class,'show']);
 
+
 Route::get('/list_of_fixed_deposit',[FixedDepoController::class,'index']);
 Route::post('/fixed_deposit',[FixedDepoController::class,'store']);
 Route::get('/user_fixed_deposit_list/{id}',[FixedDepoController::class,'show']);
+
+
+Route::get('/list_of_support_tickets',[SupportTicketController::class,'index']);
+// Route::post('/support_ticket',[SupportTicketResource::class,'store']);
+// Route::get('/user_support_ticket_list/{id}',[SupportTicketResource::class,'show']);
 
 
 Route::get('/list_of_currency',[CurrencyController::class,'index']);
